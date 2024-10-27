602ff8999dc77fa9d87ade2c6fb1796797712d12
Raunak <raunakupreti012@gmail.com>
2024-10-27 01:25:44 -0500

    Updates done

--- budgettool/src/index.js
+++ budgettool/src/index.js
@@ -5,7 +5,7 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import { Auth0Provider } from "@auth0/auth0-react";
 
 const domain = "dev-4hzy818xln1tesra.us.auth0.com";
-const clientId = "VCduGwZXae0TDF9xmCJyYLD5Cq7xW2Hj";
+const clientId = "9d48ulr4k5rNsGvsK5opNmTzGR7PMksE";
 
 ReactDOM.render(
   <Auth0Provider
--- budgettool/src/pages/signIn.js
+++ budgettool/src/pages/signIn.js
@@ -4,6 +4,12 @@ import { useAuth0 } from "@auth0/auth0-react"; // Import Auth0 hook
 const SignIn = () => {
   const { loginWithRedirect } = useAuth0(); // Get login function from Auth0
 
+  const onSignUp = () => {
+    loginWithRedirect({
+      screen_hint: "signup", // Trigger sign-up mode
+    });
+  };
+
   // Automatically redirect to Auth0 OAuth login page on component mount
   useEffect(() => {
     loginWithRedirect({
